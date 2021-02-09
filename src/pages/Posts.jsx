87c27dc4981a2c9f5d556404082958cb6acbfca7
unknown <rajovicmirko@gmail.com>
2021-02-09 09:02:53 +0100

    Add posts page search and filter elements

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,10 +1,23 @@
+import "pages/Posts.scss";
 import React from "react";
 import Layout from "components/Layout";
 
 function Posts() {
   return (
     <Layout>
-      <h1>Posts page</h1>
+      <div className="container">
+        <section className="posts-page">
+          <section className="controls">
+            <input type="text" placeholder="Search" />
+            <select>
+              <option value="">Filter by author name</option>
+            </select>
+          </section>
+          <section className="posts">
+            <h1>posts list</h1>
+          </section>
+        </section>
+      </div>
     </Layout>
   );
 }
